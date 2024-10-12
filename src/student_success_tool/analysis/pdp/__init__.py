e9680f75695afd82d87cc434f3b8c51cf059ea7b
Burton DeWilde <burtdewilde@gmail.com>
2024-10-12 14:13:24 -0400

    Add top-level modules to pdp subpkg

---
+++ src/student_success_tool/analysis/pdp/__init__.py
@@ -0,0 +1,1 @@
+from . import constants, dataio, dataops, eda, utils
---
+++ src/student_success_tool/analysis/pdp/constants.py
@@ -0,0 +1,13 @@
+DEFAULT_MIN_PASSING_GRADE = "1"
+DEFAULT_COURSE_LEVEL_PATTERN = r"^(?P<course_level>\d)\d{2}(?:[A-Z]{,2})?$"
+DEFAULT_PEAK_COVID_TERMS = {
+    ("2020-21", "SPRING"),
+    ("2020-21", "SUMMER"),
+    ("2021-22", "FALL"),
+    ("2021-22", "WINTER"),
+    ("2021-22", "SPRING"),
+    ("2021-22", "SUMMER"),
+}
+
+NUM_COURSE_FEATURE_COL_PREFIX = "num_courses"
+FRAC_COURSE_FEATURE_COL_PREFIX = "frac_courses"
