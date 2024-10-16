05b9e20f04119647e2829d50a665b03dad9ec00b
skvorekn <skvorekn>
2024-10-16 18:16:11 +0000

    fix: covid academic years off

--- src/student_success_tool/analysis/pdp/constants.py
+++ src/student_success_tool/analysis/pdp/constants.py
@@ -1,12 +1,12 @@
 DEFAULT_MIN_PASSING_GRADE = "1"
 DEFAULT_COURSE_LEVEL_PATTERN = r"^(?P<course_level>\d)\d{2}(?:[A-Z]{,2})?$"
 DEFAULT_PEAK_COVID_TERMS = {
-    ("2020-21", "SPRING"),
-    ("2020-21", "SUMMER"),
-    ("2021-22", "FALL"),
-    ("2021-22", "WINTER"),
-    ("2021-22", "SPRING"),
-    ("2021-22", "SUMMER"),
+    ("2019-20", "SPRING"), # Spring 2020
+    ("2019-20", "SUMMER"), # Summer 2020
+    ("2020-21", "FALL"), # Fall 2020
+    ("2020-21", "WINTER"), # Winter 2020/2021
+    ("2020-21", "SPRING"), # Spring 2021
+    ("2020-21", "SUMMER"), # Summer 2021
 }
 
 NUM_COURSE_FEATURE_COL_PREFIX = "num_courses"
