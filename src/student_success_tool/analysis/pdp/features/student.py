97a3b84a22e6dda9dc080838b39bf6a5d8de7185
Burton DeWilde <burtdewilde@gmail.com>
2024-10-21 21:22:03 -0400

    refactor: Streamline student features calcs
    now that num terms checkin logic removed from elsewhere -- much tidier

--- src/student_success_tool/analysis/pdp/features/student.py
+++ src/student_success_tool/analysis/pdp/features/student.py
@@ -1,6 +1,5 @@
 import functools as ft
 import logging
-import typing as t
 
 import pandas as pd
 
@@ -18,31 +17,25 @@ def add_features(df: pd.DataFrame) -> pd.DataFrame:
         df
     """
     LOGGER.info("adding student features ...")
-    feature_name_funcs: dict[str, t.Callable[[pd.DataFrame], pd.Series]] = {
-        "student_program_of_study_area_term_1": ft.partial(
+    return df.assign(
+        student_program_of_study_area_term_1=ft.partial(
             student_program_of_study_area, col="program_of_study_term_1"
         ),
-    }
-    # in case of num_terms_checkin == 1, these features can't be computed
-    # bc a necessary raw column gets dropped in the standardize function
-    if "program_of_study_year_1" in df.columns:
-        feature_name_funcs |= {
-            "student_program_of_study_area_year_1": ft.partial(
-                student_program_of_study_area, col="program_of_study_year_1"
-            ),
-            "student_program_of_study_changed_term_1_to_year_1": ft.partial(
-                student_program_of_study_changed_term_1_to_year_1,
-                term_col="program_of_study_term_1",
-                year_col="program_of_study_year_1",
-            ),
-            "student_program_of_study_area_changed_term_1_to_year_1": ft.partial(
-                student_program_of_study_changed_term_1_to_year_1,
-                term_col="student_program_of_study_area_term_1",
-                year_col="student_program_of_study_area_year_1",
-            ),
-            "diff_gpa_term_1_to_year_1": ft.partial(diff_gpa_term_1_to_year_1),
-        }
-    return df.assign(**feature_name_funcs)
+        student_program_of_study_area_year_1=ft.partial(
+            student_program_of_study_area, col="program_of_study_year_1"
+        ),
+        student_program_of_study_changed_term_1_to_year_1=ft.partial(
+            student_program_of_study_changed_term_1_to_year_1,
+            term_col="program_of_study_term_1",
+            year_col="program_of_study_year_1",
+        ),
+        student_program_of_study_area_changed_term_1_to_year_1=ft.partial(
+            student_program_of_study_changed_term_1_to_year_1,
+            term_col="student_program_of_study_area_term_1",
+            year_col="student_program_of_study_area_year_1",
+        ),
+        diff_gpa_term_1_to_year_1=ft.partial(diff_gpa_term_1_to_year_1),
+    )
 
 
 def student_program_of_study_area(df: pd.DataFrame, *, col: str) -> pd.Series:
