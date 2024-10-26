1a9fcc75452882909029f2d645a1a73d23c67d85
Burton DeWilde <burtdewilde@gmail.com>
2024-10-26 16:23:47 -0400

    Add shared year-term + dt features

--- src/student_success_tool/analysis/pdp/features/shared.py
+++ src/student_success_tool/analysis/pdp/features/shared.py
@@ -1,8 +1,24 @@
 import functools as ft
+import re
 import typing as t
 from collections.abc import Sequence
+from datetime import date
 
 import pandas as pd
+
+from .. import types
+
+RE_YEAR_TERM = re.compile(
+    r"(?P<start_yr>\d{4})-(?P<end_yr>\d{2}) (?P<term>FALL|WINTER|SPRING|SUMMER)",
+    flags=re.IGNORECASE,
+)
+
+TERM_BOUND_MONTH_DAYS = {
+    "FALL": {"start": (9, 1), "end": (12, 31)},
+    "WINTER": {"start": (1, 1), "end": (1, 31)},
+    "SPRING": {"start": (2, 1), "end": (5, 31)},
+    "SUMMER": {"start": (6, 1), "end": (8, 31)},
+}
 
 
 def extract_short_cip_code(ser: pd.Series) -> pd.Series:
@@ -44,3 +60,59 @@ def merge_many_dataframes(
     return ft.reduce(
         lambda left, right: pd.merge(left, right, on=on, how=how, sort=sort), dfs
     )
+
+
+def year_term(df: pd.DataFrame, *, year_col: str, term_col: str) -> pd.Series:
+    return df[year_col].str.cat(df[term_col], sep=" ")
+
+
+def year_term_dt(
+    df: pd.DataFrame,
+    *,
+    col: str,
+    bound: t.Literal["start", "end"],
+    first_term_of_year: types.TermType,
+) -> pd.Series:
+    """
+    Compute an approximate start/end date for a given year-term,
+    e.g. to compute time elapsed between course enrollments or to order course history.
+
+    Args:
+        df
+        col: Column in ``df`` whose values represent academic/cohort year and term,
+            formatted as "YYYY-YY TERM".
+        bound: Which bound of the date range spanned by ``year_term`` to return;
+            either the start (left) or end (right) bound.
+        first_term_of_year: Term that officially begins the institution's academic year,
+            either "FALL" or "SUMMER", which determines how the date's year is assigned.
+
+    See Also:
+        - :func:`year_term()`
+    """
+    return (
+        df[col]
+        .map(
+            ft.partial(
+                _year_term_to_dt, bound=bound, first_term_of_year=first_term_of_year
+            ),
+            na_action="ignore",
+        )
+        .astype("datetime64[s]")
+    )
+
+
+def _year_term_to_dt(
+    year_term: str, bound: t.Literal["start", "end"], first_term_of_year: types.TermType
+) -> date:
+    if match := RE_YEAR_TERM.search(year_term):
+        start_yr = int(match["start_yr"])
+        term = match["term"].upper()
+        yr = (
+            start_yr
+            if (term == "FALL" or (term == "SUMMER" and first_term_of_year == "SUMMER"))
+            else start_yr + 1
+        )
+        mo, dy = TERM_BOUND_MONTH_DAYS[term][bound]
+        return date(yr, mo, dy)
+    else:
+        raise ValueError(f"invalid year_term value: {year_term}")
--- tests/analysis/pdp/features/test_shared.py
+++ tests/analysis/pdp/features/test_shared.py
@@ -48,3 +48,76 @@ def test_frac_credits_earned(df, earned_col, attempted_col, exp):
     )
     assert isinstance(obs, pd.Series) and not obs.empty
     assert obs.equals(exp) or obs.compare(exp).empty
+
+
+@pytest.mark.parametrize(
+    ["df", "year_col", "term_col", "exp"],
+    [
+        (
+            pd.DataFrame(
+                {
+                    "year": ["20-21", "20-21", "21-22", "21-22"],
+                    "term": ["FA", "WI", "SP", "SU"],
+                }
+            ),
+            "year",
+            "term",
+            pd.Series(["20-21 FA", "20-21 WI", "21-22 SP", "21-22 SU"]),
+        )
+    ],
+)
+def test_year_term(df, year_col, term_col, exp):
+    obs = shared.year_term(df, year_col=year_col, term_col=term_col)
+    assert isinstance(obs, pd.Series) and not obs.empty
+    assert obs.equals(exp) or obs.compare(exp).empty
+
+
+@pytest.mark.parametrize(
+    ["df", "col", "bound", "first_term_of_year", "exp"],
+    [
+        (
+            pd.DataFrame(
+                {
+                    "yt": [
+                        "2020-21 FALL",
+                        "2020-21 WINTER",
+                        "2020-21 SPRING",
+                        "2020-21 SUMMER",
+                    ]
+                }
+            ),
+            "yt",
+            "start",
+            "FALL",
+            pd.Series(
+                ["2020-09-01", "2021-01-01", "2021-02-01", "2021-06-01"],
+                dtype="datetime64[ns]",
+            ),
+        ),
+        (
+            pd.DataFrame(
+                {
+                    "yt": [
+                        "2021-22 FALL",
+                        "2021-22 WINTER",
+                        "2021-22 SPRING",
+                        "2021-22 SUMMER",
+                    ]
+                }
+            ),
+            "yt",
+            "end",
+            "SUMMER",
+            pd.Series(
+                ["2021-12-31", "2022-01-31", "2022-05-31", "2021-08-31"],
+                dtype="datetime64[ns]",
+            ),
+        ),
+    ],
+)
+def test_year_term_dt(df, col, bound, first_term_of_year, exp):
+    obs = shared.year_term_dt(
+        df, col=col, bound=bound, first_term_of_year=first_term_of_year
+    )
+    assert isinstance(obs, pd.Series) and not obs.empty
+    assert obs.equals(exp) or obs.compare(exp).empty
