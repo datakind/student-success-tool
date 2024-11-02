0f57d113c48c973d04cb8714d9de4c2989b34c4f
Burton DeWilde <burtdewilde@gmail.com>
2024-11-02 13:50:50 -0400

    tests: Add tests for expanding agg features

--- tests/analysis/pdp/features/test_cumulative.py
+++ tests/analysis/pdp/features/test_cumulative.py
@@ -1,25 +1,87 @@
-# TODO: test this functionality extensively
+import numpy as np
+import pandas as pd
+import pytest
 
-# @pytest.mark.parametrize(
-#     ["df", "grp_cols", "term_rank_col", "exp"],
-#     [
-#         (
-#             pd.DataFrame(
-#                 {
-#                     "iid": ["x", "x", "x", "x", "x", "x", "x"],
-#                     "sid": ["a", "a", "b", "b", "a", "a", "b"],
-#                     "term_rank": [1, 3, 0, 1, 4, 6, 5],
-#                 }
-#             ),
-#             ["iid", "sid"],
-#             "term_rank",
-#             pd.Series([0.0, 0.333333, 0.0, 0.0, 0.25, 0.333333, 0.5]),
-#         ),
-#     ],
-# )
-# def test_compute_cumfrac_terms_unenrolled(df, grp_cols, term_rank_col, exp):
-#     obs = features.compute_cumfrac_terms_unenrolled(
-#         df, grp_cols=grp_cols, term_rank_col=term_rank_col
-#     )
-#     assert isinstance(obs, pd.Series) and not obs.empty
-#     assert pd.testing.assert_series_equal(obs, exp) is None  # raises error if not equal
+from student_success_tool.analysis.pdp.features import cumulative
+
+
+@pytest.fixture(scope="module")
+def df_grped():
+    return (
+        pd.DataFrame(
+            {
+                "student_id": ["01", "01", "01", "01", "01"],
+                "academic_year": [
+                    "2020-21",
+                    "2020-21",
+                    "2020-21",
+                    "2021-22",
+                    "2021-22",
+                ],
+                "academic_term": ["FALL", "SPRING", "SUMMER", "FALL", "SPRING"],
+                "term_id": [
+                    "2020-21 FALL",
+                    "2020-21 SPRING",
+                    "2020-21 SUMMER",
+                    "2021-22 FALL",
+                    "2021-22 SPRING",
+                ],
+                "course_grade_num_mean": [4.0, 2.5, 1.75, 3.25, 3.5],
+                "num_courses": [3, 2, 2, 2, 1],
+                "num_courses_course_level_0": [2, 1, 0, 0, 0],
+                "num_courses_course_level_1": [1, 1, 2, 2, 1],
+            }
+        )
+        .sort_values(by=["student_id", "academic_year", "academic_term"])
+        .groupby(by="student_id", as_index=True, observed=True, sort=False)
+    )
+
+
+@pytest.mark.parametrize(
+    ["num_course_cols", "col_aggs", "exp"],
+    [
+        (
+            ["num_courses_course_level_0", "num_courses_course_level_1"],
+            [
+                ("term_id", "count"),
+                ("course_grade_num_mean", ["mean", "std"]),
+                ("num_courses", "sum"),
+            ],
+            pd.DataFrame(
+                {
+                    "term_id_cumcount": [1.0, 2.0, 3.0, 4.0, 5.0],
+                    "course_grade_num_mean_cummean": [4.0, 3.25, 2.75, 2.875, 3.0],
+                    "course_grade_num_mean_cumstd": [
+                        np.nan,
+                        1.061,
+                        1.146,
+                        0.968,
+                        0.884,
+                    ],
+                    "num_courses_cumsum": [3.0, 5.0, 7.0, 9.0, 10.0],
+                    "num_courses_course_level_0_cumfrac": [
+                        0.667,
+                        0.6,
+                        0.429,
+                        0.333,
+                        0.3,
+                    ],
+                    "num_courses_course_level_1_cumfrac": [
+                        0.333,
+                        0.4,
+                        0.571,
+                        0.667,
+                        0.7,
+                    ],
+                }
+            ),
+        ),
+    ],
+)
+def test_expanding_agg_features(df_grped, num_course_cols, col_aggs, exp):
+    obs = cumulative.expanding_agg_features(
+        df_grped, num_course_cols=num_course_cols, col_aggs=col_aggs
+    )
+    assert isinstance(obs, pd.DataFrame) and not obs.empty
+    # raises error if not equal
+    assert pd.testing.assert_frame_equal(obs, exp, rtol=0.001) is None
