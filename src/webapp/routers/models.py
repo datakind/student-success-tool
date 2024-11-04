4603fddb3263254a8bba9c506dc25f5d1fc3eeed
crysg <86477109+crysg@users.noreply.github.com>
2024-11-04 09:34:37 -0500

    Update models.py

--- src/webapp/routers/models.py
+++ src/webapp/routers/models.py
@@ -1,2 +1,94 @@
 """API functions related to models.
 """
+
+# Model related operations. Or model specific data.
+
+@app.get("/institutions/{inst_id}/models")
+def read_inst_models(
+    current_user: Annotated[BaseUser],
+):
+    """Returns top-level view of all models attributed to a given institution.
+    
+    Only visible to data owners of that institution or higher.
+
+    Args:
+        current_user: the user making the request.
+    """
+    has_access_to_inst_or_err(inst_id, current_user)
+    has_full_data_access_or_err(current_user, "models")
+    return ""
+
+@app.get("/institutions/{inst_id}/models/{model_id}")
+def read_inst_model(
+    current_user: Annotated[BaseUser],
+):
+    """Returns a specific model's details e.g. model card.
+    
+    Only visible to data owners of that institution or higher.
+
+    Args:
+        current_user: the user making the request.
+    """
+    has_access_to_inst_or_err(inst_id, current_user)
+    has_full_data_access_or_err(current_user, "this model")
+    return ""
+
+@app.get("/institutions/{inst_id}/models/{model_id}/vers")
+def read_inst_model_versions(
+    current_user: Annotated[BaseUser],
+):
+    """Returns all versions of a given model.
+    
+    Only visible to data owners of that institution or higher. This can include retrained models etc.
+
+    Args:
+        current_user: the user making the request.
+    """
+    has_access_to_inst_or_err(inst_id, current_user)
+    has_full_data_access_or_err(current_user, "this model")
+    
+    return ""
+
+@app.get("/institutions/{inst_id}/models/{model_id}/vers/{vers_id}")
+def read_inst_model_version(
+    current_user: Annotated[BaseUser],
+):
+    """Returns details around a version of a given model.
+    
+    Only visible to data owners of that institution or higher.
+
+    Args:
+        current_user: the user making the request.
+    """
+    has_access_to_inst_or_err(inst_id, current_user)
+    has_full_data_access_or_err(current_user, "this model")
+    return ""
+
+@app.get("/institutions/{inst_id}/models/{model_id}/output")
+def read_inst_model_outputs(
+    current_user: Annotated[BaseUser],
+):
+    """Returns top-level info around all executions of a given model.
+    
+    Only visible to users of that institution or Datakinder access types.
+
+    Args:
+        current_user: the user making the request.
+    """
+    has_access_to_inst_or_err(inst_id, current_user)
+    return ""
+
+@app.get("/institutions/{inst_id}/models/{model_id}/output/{output_id}")
+def read_inst_model_output(
+    current_user: Annotated[BaseUser],
+):
+    """Returns a given executions of a given model.
+    
+    Only visible to users of that institution or Datakinder access types.
+    If a viewer has record allowlist restrictions applied, only those records are returned.
+
+    Args:
+        current_user: the user making the request.
+    """
+    has_access_to_inst_or_err(inst_id, current_user)
+    return ""
