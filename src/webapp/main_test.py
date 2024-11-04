02808eabb91b88f8c19e2b221b40d6cbd25a6507
crysg <86477109+crysg@users.noreply.github.com>
2024-11-04 13:46:32 -0500

    Add main_test.py skeleton
    #gcp-build

--- src/webapp/main_test.py
+++ src/webapp/main_test.py
@@ -1,2 +1,24 @@
 """Test file for the main.py file and constituent API functions.
 """
+
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+
+def test_read_all_inst():
+    response = client.get("/institutions")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "foo",
+        "title": "Foo",
+        "description": "There goes my hero",
+    }
+
+
+def test_read_inst():
+    response = client.get("/institutions/123")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Item not found"}
