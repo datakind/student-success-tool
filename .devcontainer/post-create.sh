6fb4ab2fb925cc7d8418a10c97078b50137f9c91
Jamal Carvalho <jamalcarvalho@google.com>
2024-11-08 09:19:13 -0500

    add gcloud cli to devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,1 +1,11 @@
-uv sync --frozen --no-install-project --no-dev
+#!/usr/bin/env bash
+
+source devtools/lib.sh
+
+# Install dependencies
+runcmd uv sync --frozen --no-install-project --no-dev
+
+# Install gcloud cli
+runcmd gcloud init --skip-diagnostics
+
+runcmd gcloud auth application-default login --impersonate-service-account local-webapp@dev-sst-439514.iam.gserviceaccount.com
