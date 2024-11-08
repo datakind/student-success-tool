61f8eaee798e998d9b2f11ad9fc250b86b8e73fa
Jamal Carvalho <18269441+jamalc@users.noreply.github.com>
2024-11-08 10:01:43 -0500

    run formatter

--- scripts/set_bucket_cors_policy.py
+++ scripts/set_bucket_cors_policy.py
@@ -3,7 +3,9 @@ from google.cloud import storage
 from typing import List
 
 
-def cors_configuration(bucket_name: str, origin: str, methods: List[str] = ['PUT', 'POST']) -> storage.Bucket:
+def cors_configuration(
+    bucket_name: str, origin: str, methods: List[str] = ["PUT", "POST"]
+) -> storage.Bucket:
     """Set a bucket's CORS policies configuration."""
 
     storage_client = storage.Client()
@@ -11,11 +13,9 @@ def cors_configuration(bucket_name: str, origin: str, methods: List[str] = ['PUT
     bucket.cors = [
         {
             "origin": [origin],
-            "responseHeader": [
-                "Content-Type",
-                "x-goog-resumable"],
+            "responseHeader": ["Content-Type", "x-goog-resumable"],
             "method": methods,
-            "maxAgeSeconds": 3600
+            "maxAgeSeconds": 3600,
         }
     ]
     bucket.patch()
@@ -27,7 +27,6 @@ def main():
     bucket_name, origin = sys.argv[1], sys.argv[2]
     bucket = cors_configuration(bucket_name, origin).cors
     print(f"Set CORS policies for bucket {bucket.name} is {bucket.cors}")
-
 
 
 if __name__ == "__main__":
