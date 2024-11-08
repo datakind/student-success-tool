0edb82d68e498ab1b16025db6be68bb90dcbe99b
Jamal Carvalho <18269441+jamalc@users.noreply.github.com>
2024-11-08 15:24:24 +0000

    add github login to post create step

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,7 +5,9 @@ source devtools/lib.sh
 # Install dependencies
 runcmd uv sync --frozen --no-install-project --no-dev
 
-# Install gcloud cli
+# Init gcloud cli
 runcmd gcloud init --skip-diagnostics
-
 runcmd gcloud auth application-default login --impersonate-service-account local-webapp@dev-sst-439514.iam.gserviceaccount.com
+
+# Init gh cli
+runcmd gh auth login
