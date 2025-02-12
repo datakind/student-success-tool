b829562877b897c64c99337bccf5863ccb4c8e93
nm3224 <nm3224>
2025-02-12 18:33:21 +0000

    finalizing public repo contribution

--- src/student_success_tool/analysis/pdp/targets/failure_to_graduate_within_x_years.py
+++ src/student_success_tool/analysis/pdp/targets/failure_to_graduate_within_x_years.py
@@ -1,7 +1,7 @@
 import logging
 import typing as t
 from collections.abc import Collection
-# import numpy as np
+import numpy as np
 import pandas as pd
 from student_success_tool.analysis.pdp.targets import shared
 from student_success_tool.analysis.pdp import utils, features
@@ -32,7 +32,7 @@ def make_labeled_dataset(
     See Also:
         - :func:`select_eligible_students()`
         - :func:`compute_target_variable()`
-        - :func:`shared.get_first_student_terms_within_cohort()`
+        - :func:`shared.get_nth_student_terms()`
     """
     df_eligible_students = select_eligible_students(
         df,
@@ -63,52 +63,74 @@ def make_labeled_dataset(
         df_eligible_student_terms,
         intensity_time_lefts=intensity_time_lefts,
         student_id_cols=student_id_cols,
+        num_terms_in_year=num_terms_in_year,
         enrollment_intensity_col=enrollment_intensity_col,
         years_to_degree_col=years_to_degree_col,
-        num_terms_in_year=num_terms_in_year,
     )
     df_labeled = pd.merge(df_features, df_targets, on=student_id_cols, how="inner")
     return df_labeled
 
 
-def compute_target_variable(    
-    df: pd.DataFrame,    
-    *,    
-    student_id_cols: str | list[str] = "student_guid",    
-    years_to_degree_col: str = "first_year_bach",  
-    intensity_time_lefts: list[tuple[str, float, t.Literal["year", "term"]]],   
-    intensity_col: str = "intensity_type",  
-    enrollment_intensity_col: str = "enrollment_intensity_first_term",
+def compute_target_variable(
+    df: pd.DataFrame,
+    *,
+    intensity_time_lefts: list[tuple[str, float, t.Literal["year", "term"]]],
+    student_id_cols: str | list[str] = "student_guid",
     num_terms_in_year: int = 4,
-) -> pd.Series:   
-    """  
-    Args:  
-        df  
-        student_id_cols  
-        years_to_degree_col  
-        intensity_time_lefts  
-        intensity_col  
-    """   
-    student_id_cols = utils.to_list(student_id_cols)  
-    intensity_num_terms = {intensity: time if unit == "year" else time / num_terms_in_year  
-                           for intensity, time, unit in intensity_time_lefts  
-                           }   
-    return (    
-        df[student_id_cols + [years_to_degree_col, enrollment_intensity_col]]    
-        .drop_duplicates(subset=student_id_cols, ignore_index=True)    
-        .assign(target=lambda df: 
-            (
-                # didn't graduate within 4 years
-                df[years_to_degree_col].gt(df[enrollment_intensity_col].map(intensity_num_terms)) 
-                
-                # or they never graduated
-                | (df[years_to_degree_col].isna())
+    enrollment_intensity_col: str = "enrollment_intensity_first_term",
+    years_to_degree_col: str = "first_year_to_bachelors_at_cohort_inst",
+) -> pd.Series:
+    """
+    Args:
+        df
+        intensity_time_limits: Mapping of enrollment intensity value (e.g. "FULL-TIME")
+            to the maximum number of years allowed to earn degree "in time" (e.g. 4).
+        student_id_cols
+        enrollment_intensity_col
+        years_to_degree_col: Name of column giving the year in which students _first_
+            earned a particular degree (by default, a Bachelor's degree).
+        num_terms_in_year: Defined number of terms in one academic year.
+    """
+    student_id_cols = utils.to_list(student_id_cols)
+    include_cols = student_id_cols + [enrollment_intensity_col, years_to_degree_col]
+    # we want a target for every student in input df; this will ensure it
+    df_distinct_students = df[student_id_cols].drop_duplicates(ignore_index=True)
+    df_first_terms = shared.get_first_student_terms(
+        df, student_id_cols=student_id_cols, include_cols=include_cols
+    )
+    intensity_time_limits = {
+        intensity: time if unit == "year" else time / num_terms_in_year
+        for intensity, time, unit in intensity_time_lefts
+    }
+    # compute all intensity/year boolean arrays separately
+    # then combine with a logical OR
+    targets = [
+        (
+            # enrollment intensity is equal to a specified value
+            df_first_terms[enrollment_intensity_col].eq(intensity)
+            & (
+                # they graduated after max num years allowed
+                (df_first_terms[years_to_degree_col]).gt(num_years)
+                # or they *never* graduated
+                | df_first_terms[years_to_degree_col].isna()
             )
-        )        
-        .astype({"target": "bool"})    
-        .set_index(student_id_cols)    
-        .loc[:, "target"]    
-    ) 
+        )
+        for intensity, num_years in intensity_time_limits.items()
+    ]
+    target = np.logical_or.reduce(targets)
+    df_target_true = (
+        df_first_terms.loc[target, student_id_cols]
+        .assign(target=True)
+        .astype({"target": "boolean"})
+    )
+    return (
+        # all students not assigned True, now assigned False
+        pd.merge(df_distinct_students, df_target_true, on=student_id_cols, how="left")
+        .fillna(False)
+        .astype({"target": "bool"})
+        .set_index(student_id_cols)
+        .loc[:, "target"]
+    )
 
 def select_eligible_students(  
     df: pd.DataFrame,
