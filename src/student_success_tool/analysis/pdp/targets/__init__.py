572ab30d17674e06e0164e243631c4cda5460a78
nm3224 <nm3224>
2025-02-13 03:23:40 +0000

    adding to init file!!

--- src/student_success_tool/analysis/pdp/targets/__init__.py
+++ src/student_success_tool/analysis/pdp/targets/__init__.py
@@ -2,5 +2,6 @@ from . import (
     failure_to_earn_enough_credits_in_time_from_checkin,
     failure_to_earn_enough_credits_in_time_from_enrollment,
     failure_to_retain,
+    failure_to_graduate_within_x_years,
     shared,
 )
