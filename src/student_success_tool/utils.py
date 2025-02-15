61ef9bbc559340d5bfc36c426233a105f0bdd35b
Burton DeWilde <burtdewilde@gmail.com>
2025-02-15 10:30:38 -0500

    add util func to get db widgets

--- src/student_success_tool/utils.py
+++ src/student_success_tool/utils.py
@@ -1,6 +1,9 @@
+import logging
 import re
 import typing as t
 from collections.abc import Collection, Iterable
+
+LOGGER = logging.getLogger(__name__)
 
 RE_VARIOUS_PUNCTS = re.compile(r"[!()*+\,\-./:;<=>?[\]^_{|}~]")
 RE_QUOTATION_MARKS = re.compile(r"[\'\"\`]")
@@ -40,3 +43,24 @@ def convert_to_snake_case(col: str) -> str:
         r"([A-Z][a-z]+)", r" \1", re.sub(r"([A-Z]+|[0-9]+|\W+)", r" \1", col)
     ).split()
     return "_".join(words).lower()
+
+
+def get_db_widget_param(name: str, *, default: t.Optional[object] = None) -> object:
+    """
+    Get a Databricks widget parameter by ``name``,
+    returning a ``default`` value if not found.
+
+    References:
+        - https://docs.databricks.com/en/dev-tools/databricks-utils.html#dbutils-widgets-get
+    """
+    # these only work in a databricks env, so hide them here
+    from databricks.sdk.runtime import dbutils
+    from py4j.protocol import Py4JJavaError
+
+    try:
+        return dbutils.widgets.get(name)
+    except Py4JJavaError:
+        LOGGER.warning(
+            "no db widget found with name=%s; returning default=%s", name, default
+        )
+        return default
