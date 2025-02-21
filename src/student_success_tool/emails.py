5b88b0fe90a7c83b64b4fffb901c5bb00327be7e
Crystal Ren <crystalren97@gmail.com>
2025-02-21 09:58:41 -0500

    add emails utils file

---
+++ src/student_success_tool/emails.py
@@ -0,0 +1,81 @@
+""" Utility functions for sending emails in Databricks.
+"""
+
+import smtplib
+
+from email.message import EmailMessage
+from email.headerregistry import Address
+from email.utils import make_msgid
+from google.cloud import storage
+
+SMTP_SERVER = "smtp.mandrillapp.com"
+# TODO: switch port?
+SMTP_PORT = 587  # or 465 for SSL
+COMPLETION_SUCCESS_SUBJECT = "Student Success Tool: Inference Results Available."
+COMPLETION_SUCCESS_MESSAGE = """\
+    Hello!
+
+    Your Datakind Student Success Tool inference run has successfully completed and the results are ready for viewing on the Website.
+    """
+
+INFERENCE_KICKOFF_SUBJECT = "Student Success Tool: Inference Run In Progress."
+INFERENCE_KICKOFF_MESSAGE = """\
+    Hello!
+
+    Your Datakind Student Success Tool inference run has been triggered. Once results have been checked and are ready for viewing, you'll receive another email.
+    """
+
+
+def send_email(
+    sender_email,
+    receiver_email_list,
+    cc_email_list,
+    subject,
+    body,
+    mandrill_username,
+    mandrill_password,
+):
+    # Create the email message
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = sender_email
+    msg["To"] = receiver_email_list
+    msg["Cc"] = cc_email_list
+    msg.set_content(body)
+    try:
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.ehlo()
+        server.starttls()
+        server.login(mandrill_username, mandrill_password)
+        server.send_message(msg)
+        server.quit()
+    except Exception as e:
+        raise e
+
+
+def send_completion_email(
+    sender_email, receiver_email_list, cc_email_list, username, password
+):
+    send_email(
+        sender_email,
+        receiver_email_list,
+        cc_email_list,
+        COMPLETION_SUCCESS_SUBJECT,
+        COMPLETION_SUCCESS_MESSAGE,
+        username,
+        password,
+    )
+
+
+def send_inference_kickoff_email(
+    sender_email, receiver_email_list, cc_email_list, username, password
+):
+    send_email(
+        sender_email,
+        receiver_email_list,
+        cc_email_list,
+        INFERENCE_KICKOFF_SUBJECT,
+        INFERENCE_KICKOFF_MESSAGE,
+        username,
+        password,
+    )
