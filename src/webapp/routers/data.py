4caf17ca7feb88bccd90e69df8bc8470d5556e92
crysg <86477109+crysg@users.noreply.github.com>
2025-02-27 18:21:42 -0500

    Update data.py

--- src/webapp/routers/data.py
+++ src/webapp/routers/data.py
@@ -850,7 +850,6 @@ def download_url_inst_file(
                     FileTable.name == file_name,
                     FileTable.inst_id == str_to_uuid(inst_id),
                     FileTable.sst_generated,
-                    not FileTable.deleted,
                 )
             )
         )
@@ -867,6 +866,11 @@ def download_url_inst_file(
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="File duplicates found.",
+        )
+    if query_result[0][0].deleted:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="File has been deleted.",
         )
     return storage_control.generate_download_signed_url(
         get_external_bucket_name(inst_id), file_name
