35aa56dc84fcfa5648d637d0931c0a8cb66f2537
vishpillai123 <vishpillai123>
2025-03-07 15:12:47 +0000

    reformatted code per ruff

--- src/student_success_tool/modeling/inference.py
+++ src/student_success_tool/modeling/inference.py
@@ -88,8 +88,13 @@ def select_top_features_for_display(
     top_features_df = pd.DataFrame(top_features_info)
 
     # Ensure top features are in descending order of importance
-    assert (all(abs(top_features_df[f'Feature_{i}_Importance']) >= abs(top_features_df[f'Feature_{i+1}_Importance'])) 
-            for i in range(1, 5)), "Final output has invalid SHAP values across top 5 ranked features."
+    assert (
+        all(
+            abs(top_features_df[f"Feature_{i}_Importance"])
+            >= abs(top_features_df[f"Feature_{i+1}_Importance"])
+        )
+        for i in range(1, 5)
+    ), "Final output has invalid SHAP values across top 5 ranked features."
     
     return top_features_df
 
