9381068bd5e2386eaf0f89785d301ef295450f7f
Burton DeWilde <burtdewilde@gmail.com>
2025-03-11 13:02:18 -0400

    [pdp] improve term features (#101)
    * add core terms features input param
    
    * add term-is-core feature w/ unit tests
    
    * propagate term-is-core feature through
    
    * further pipe core term features through code
    
    * finish replace fall-spring term feats w/ core
    
    * add core terms to features table
    
    * docs: add clarification re core terms
    
    * add non-core term cumulative feature
    
    * adds core terms to project config

--- src/student_success_tool/features/pdp/student_term.py
+++ src/student_success_tool/features/pdp/student_term.py
@@ -53,9 +53,11 @@ def aggregate_from_course_level_features(
         academic_term=("academic_term", "first"),
         term_start_dt=("term_start_dt", "first"),
         term_rank=("term_rank", "first"),
+        term_rank_core=("term_rank_core", "first"),
+        term_rank_noncore=("term_rank_noncore", "first"),
+        term_is_core=("term_is_core", "first"),
+        term_is_noncore=("term_is_noncore", "first"),
         term_in_peak_covid=("term_in_peak_covid", "first"),
-        term_rank_fall_spring=("term_rank_fall_spring", "first"),
-        term_is_fall_spring=("term_is_fall_spring", "first"),
         term_program_of_study=("term_program_of_study", "first"),
     )
     # various aggregations, with an eye toward cumulative features downstream
