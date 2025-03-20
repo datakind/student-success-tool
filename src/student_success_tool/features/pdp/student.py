1332c051bcff915b4d5c63d28d21a47717f269d1
nm3224 <nm3224>
2025-03-20 20:22:21 +0000

    changing single to double quotes for linter

--- src/student_success_tool/features/pdp/student.py
+++ src/student_success_tool/features/pdp/student.py
@@ -89,6 +89,8 @@ def diff_gpa_term_1_to_year_1(
 
 
 def student_is_pell_recipient_first_year(
-    df: pd.DataFrame, *, pell_col: str = "pell_status_first_year"
+    df: pd.DataFrame, 
+    *, 
+    pell_col: str = "pell_status_first_year",
 ) -> pd.Series:
-    return df[pell_col].map({'Y': True, 'N': False})
+    return df[pell_col].map({"Y": True, "N": False})
