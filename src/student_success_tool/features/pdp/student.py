add9d0d45c30a1e720ec09dca91a55d5d9ca66c0
nm3224 <nm3224>
2025-03-20 20:19:07 +0000

    changing structure for linting format

--- src/student_success_tool/features/pdp/student.py
+++ src/student_success_tool/features/pdp/student.py
@@ -88,8 +88,5 @@ def diff_gpa_term_1_to_year_1(
     return df[year_col].sub(df[term_col])
 
 
-def student_is_pell_recipient_first_year(
-    df: pd.DataFrame,
-    *,
-    pell_col: str = "pell_status_first_year") -> pd.Series:
+def student_is_pell_recipient_first_year(df: pd.DataFrame, *, pell_col: str = "pell_status_first_year") -> pd.Series:
     return df[pell_col].map({'Y': True, 'N': False})
