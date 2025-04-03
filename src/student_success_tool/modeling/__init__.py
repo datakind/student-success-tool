a928fe40b750475f581174dafe4791d619dcc48f
vishpillai123 <vishpillai123>
2025-04-03 14:54:39 +0000

    adding to init file

--- src/student_success_tool/modeling/__init__.py
+++ src/student_success_tool/modeling/__init__.py
@@ -1,1 +1,1 @@
-from . import evaluation, feature_selection, inference, registration, training, utils
+from . import evaluation, bias_detection, feature_selection, inference, registration, training, utils
