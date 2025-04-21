060596ce47e0cff218d4cfbb6d493e107dcf048e
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-04-20 18:00:08 -1000

    Edit cumulative function and ruff formatting

--- src/student_success_tool/preprocessing/features/pdp/cumulative.py
+++ src/student_success_tool/preprocessing/features/pdp/cumulative.py
@@ -149,15 +149,14 @@ def expanding_agg_features(
                 f"{dummy_course}_cummax"
                 for dummy_course in dummy_course_cols
             ]
-        within_cols = []
-        for col in action_cols:
-            within_col = f"{col}_within_{credits}_credits"
-            # df_cumaggs[within_col] = (df_cumaggs[col].astype(bool)) & (df_cumaggs["num_credits_earned_cumsum"] <= credits)
-            df_cumaggs[within_col] = (df_cumaggs[col].astype(bool)) 
-            within_cols.append(within_col)
+        action_status_df = pd.DataFrame(index=df_cumaggs.index)
 
-        action_status_df = df_cumaggs[within_cols].groupby(level=df_cumaggs.index.names).max()
-        concat_dfs = [concat_dfs, action_status_df]
+        for col in action_cols:
+            within_col = f"{col}_in_{credits}_creds"
+            action_status_df[within_col] = (df_cumaggs[col].astype(bool)) & (df_cumaggs["num_credits_earned_cumsum"] <= credits)
+
+        action_status_df = action_status_df.groupby(level=df_cumaggs.index.names).transform("max")
+        concat_dfs.append(action_status_df)
 
     return (
         # append cum-frac features to all cum-agg features
