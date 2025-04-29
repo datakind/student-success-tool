0917fba27fa08f36dd3759832bfef93bab791d02
vishpillai123 <vishpillai123>
2025-04-29 00:31:26 +0000

    adding full type hint

--- src/student_success_tool/reporting/utils/utils.py
+++ src/student_success_tool/reporting/utils/utils.py
@@ -8,7 +8,7 @@ import pathlib
 def embed_image(description: str, local_path: str | pathlib.Path, width: int) -> str:
     return f'<img src="{os.path.relpath(local_path, start=os.getcwd())}" alt="{description}" width="{width}">'
 
-def list_artifacts(run_id: str, folder: str)
+def list_artifacts(run_id: str, folder: str) -> list[mlflow.entities.FileInfo]:
     return mlflow.artifacts.list_artifacts(self.run_id, path=folder)
 
 def download_artifact(run_id: str, description: str, artifact_path: str, width: int, local_folder: str) -> str:
