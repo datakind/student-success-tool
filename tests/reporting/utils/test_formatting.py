d2d88501560ca5a8aeb214a7699970678a983577
vishpillai123 <vishpillai123>
2025-05-01 16:28:45 +0000

    adding tests

---
+++ tests/reporting/utils/test_formatting.py
@@ -0,0 +1,28 @@
+import pytest
+from student_success_tool.reporting.utils.formatting import Formatting
+
+@pytest.fixture
+def formatter():
+    return Formatting()
+
+@pytest.mark.parametrize("input_text,expected", [
+    ("bachelor's degree", "Bachelor's Degree"),
+    ("full-time", "Full-Time"),
+    ("associate's-degree", "Associate's-Degree"),
+    ("first_generation", "First Generation"),
+    ("1st_semester". "1st Semester")
+    ("STEM", "Stem"),  # no acronym preservation
+])
+def test_friendly_case_capitalize_true(formatter, input_text, expected):
+    result = formatter.friendly_case(input_text)
+    assert result == expected
+
+@pytest.mark.parametrize("input_text,expected", [
+    ("bachelor's degree", "bachelor's degree"),
+    ("full-time", "full-time"),
+    ("first_term", "first term"),
+    ("1st_semester", "1st semester")
+])
+def test_friendly_case_capitalize_false(formatter, input_text, expected):
+    result = formatter.friendly_case(input_text, capitalize=False)
+    assert result == expected
