415668185bb3a7495ffe47268b5ac8a94323c9f7
Burton DeWilde <burtdewilde@gmail.com>
2025-05-03 13:08:28 -0400

    update num credits target w/ null val logic

--- src/student_success_tool/preprocessing/targets/pdp/retention.py
+++ src/student_success_tool/preprocessing/targets/pdp/retention.py
@@ -21,14 +21,17 @@ def compute_target(
 
     Args:
         df: Student-term dataset.
-        max_academic_year
+        max_academic_year: Maximum academic year in the full dataset , either inferred
+            from ``df[term_id_col]`` itself or as a manually specified value which
+            may be different from the actual max value in ``df`` , depending on use case.
+            Note: Value must be a string formatted as "YYYY[-YY]".
         student_id_cols: One or multiple columns uniquely identifying students.
         retention_col: Column whose values indicate whether or not a given student
             was retained, either as booleans or 0/1 integers.
             Note: If ``df`` has one row per student-term, it's assumed that retention
             values are the same across all rows, and we simply use the first.
-        cohort_id_col
-        term_id_col
+        cohort_id_col: Column used to uniquely identify student cohorts.
+        term_id_col: Colum used to uniquely identify academic terms.
 
     References:
         - https://help.studentclearinghouse.org/pdp/knowledge-base/cohort-level-analysis-ready-file-data-dictionary/
