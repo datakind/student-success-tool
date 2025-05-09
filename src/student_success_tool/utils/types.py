42751aafa659d35caea34acea1f319abb7f234af
Burton DeWilde <burtdewilde@gmail.com>
2025-05-09 10:27:07 -0400

    [pdp] structure checkpoint configs (#153)
    * add checkpoint type type
    
    (yes: type type)
    
    * add structured configs for checkpoints
    
    * update template + tests for ckpt configs
    
    * repeat types trick w/ targets in proj cfg

--- src/student_success_tool/utils/types.py
+++ src/student_success_tool/utils/types.py
@@ -3,6 +3,15 @@ from collections.abc import Collection, Iterable
 
 TermType = t.Literal["FALL", "WINTER", "SPRING", "SUMMER"]
 IntensityTimeLimitsType = dict[str, tuple[float, t.Literal["year", "term"]]]
+CheckpointTypeType = t.Literal[
+    "nth",
+    "first",
+    "last",
+    "first_at_num_credits_earned",
+    "first_within_cohort",
+    "last_in_enrollment_year",
+]
+TargetTypeType = t.Literal["graduation", "retention", "credits_earned"]
 
 
 def to_list(value: t.Any) -> list:
