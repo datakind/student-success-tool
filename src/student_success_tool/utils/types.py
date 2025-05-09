8ea7f5c3f2ded449178e407fa4791f8404af5a3f
Burton DeWilde <burtdewilde@gmail.com>
2025-05-08 21:38:08 -0400

    add checkpoint type type
    (yes: type type)

--- src/student_success_tool/utils/types.py
+++ src/student_success_tool/utils/types.py
@@ -3,6 +3,14 @@ from collections.abc import Collection, Iterable
 
 TermType = t.Literal["FALL", "WINTER", "SPRING", "SUMMER"]
 IntensityTimeLimitsType = dict[str, tuple[float, t.Literal["year", "term"]]]
+CheckpointTypeType = t.Literal[
+    "nth",
+    "first",
+    "last",
+    "first_at_num_credits_earned",
+    "first_within_cohort",
+    "last_in_enrollment_year",
+]
 
 
 def to_list(value: t.Any) -> list:
