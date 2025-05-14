657aeb7e3cc8f46a47af0be3882c190ec597bae1
vishpillai123 <vishpillai123>
2025-05-14 17:25:33 +0000

    reverting

--- pipelines/institution_id/00-data-assessment-TEMPLATE.py
+++ pipelines/institution_id/00-data-assessment-TEMPLATE.py
@@ -97,10 +97,8 @@ cfg
 # TODO: if dataset info included in cfg, specify its name here
 dataset_name = "DATASET_NAME"
 
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ## course dataset
+%md
+## course dataset
 
 # COMMAND ----------
 
