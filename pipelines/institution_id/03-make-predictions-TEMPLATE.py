3d827bc705416582905ae9a30e5917eb588f9418
vishpillai123 <vishpillai123>
2025-05-16 21:58:42 +0000

    putting formatting in a functino

--- pipelines/institution_id/03-make-predictions-TEMPLATE.py
+++ pipelines/institution_id/03-make-predictions-TEMPLATE.py
@@ -298,7 +298,7 @@ shap.summary_plot(
 )
 shap_fig = plt.gcf()
 # save shap summary plot via mlflow into experiment artifacts folder
-with mlflow.start_run(run_id=cfg.models[model_name].run_id) as run:
+with mlflow.start_run(run_id=cfg.model.run_id) as run:
     mlflow.log_figure(
         shap_fig, f"shap_summary_{dataset_name}_dataset_{df_ref.shape[0]}_ref_rows.png"
     )
