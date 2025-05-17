3c90262e0564d1ecce7c61c48b37ac643b398ff0
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-05-17 11:06:00 -0700

    Update pdp_data_ingestion.py

--- pipelines/tasks/pdp_data_ingestion/pdp_data_ingestion.py
+++ pipelines/tasks/pdp_data_ingestion/pdp_data_ingestion.py
@@ -295,10 +295,14 @@ if __name__ == "__main__":
     sys.path.append(args.custom_schemas_path)
     try:
         print("sys.path:", sys.path)
+        path = "/Workspace/Users/kayla@datakind.org/sst-private/kentucky_state_uni/schemas.py"
+        print("Exists:", os.path.exists(path))
+        print("Listdir:", os.listdir("/Workspace/Users/kayla@datakind.org/sst-private/kentucky_state_uni"))
         schemas = importlib.import_module(f"{args.databricks_institution_name}.schemas")
         logging.info("Running task with custom schema")
     except Exception:
         print("Running task with default schema")
+        print('Exception', Exception)
         from student_success_tool.dataio.schemas import pdp as schemas
         logging.info("Running task with default schema")
     try:
