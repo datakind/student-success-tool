40546e0547e94e5b3470202b84e22b4d57f6f0e9
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-05-19 08:04:25 -0700

    Update pdp_model_inference.py

--- pipelines/tasks/pdp_model_inference/pdp_model_inference.py
+++ pipelines/tasks/pdp_model_inference/pdp_model_inference.py
@@ -33,7 +33,7 @@ from email.headerregistry import Address
 # Import project-specific modules
 import student_success_tool.dataio as dataio
 from student_success_tool.modeling import inference
-from student_success_tool.schemas.pdp import PDPProjectConfig
+from student_success_tool.configs.pdp import PDPProjectConfig
 from student_success_tool.modeling.evaluation import plot_shap_beeswarm
 from student_success_tool.utils import emails
 
@@ -82,7 +82,7 @@ class ModelInferenceTask:
 
     def load_mlflow_model(self):
         """Loads the MLflow model."""
-        model_uri = f"runs:/{self.cfg.models['graduation'].run_id}/model"
+        model_uri = f"runs:/{self.cfg.model.run_id}/model"
 
         try:
             load_model_func = {
