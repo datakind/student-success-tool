f947bd44d68ba3b155aa5ba6449035846180ddc9
nm3224 <nm3224>
2025-05-28 21:07:47 +0000

    keeping tests

--- tests/preprocessing/checkpoints/test_pdp.py
+++ tests/preprocessing/checkpoints/test_pdp.py
@@ -4,63 +4,63 @@ import pytest
 from student_success_tool.preprocessing.checkpoints import pdp
 
 
-# @pytest.fixture(scope="module")
-# def df_test():
-#     return pd.DataFrame(
-#         {
-#             "student_id": ["01", "01", "01", "02", "02", "03", "04", "05"],
-#             "term_rank": [3, 4, 5, 5, 6, 2, 4, 8],
-#             "cohort_id": [
-#                 "2020-21 SPRING",
-#                 "2020-21 SPRING",
-#                 "2020-21 SPRING",
-#                 "2021-22 FALL",
-#                 "2021-22 FALL",
-#                 "2019-20 FALL",
-#                 "2020-21 SPRING",
-#                 "2022-23 FALL",
-#             ],
-#             "term_id": [
-#                 "2020-21 FALL",
-#                 "2020-21 SPRING",
-#                 "2021-22 FALL",
-#                 "2021-22 FALL",
-#                 "2023-24 FALL",
-#                 "2019-20 SPRING",
-#                 "2020-21 SPRING",
-#                 "2022-23 FALL",
-#             ],
-#             "enrollment_year": [1, 1, 2, 1, 3, 1, 1, 1],
-#             "enrollment_intensity": [
-#                 "FULL-TIME",
-#                 "FULL-TIME",
-#                 "FULL-TIME",
-#                 "PART-TIME",
-#                 "PART-TIME",
-#                 "PART-TIME",
-#                 "FULL-TIME",
-#                 pd.NA,
-#             ],
-#             "num_credits_earned": [25.0, 30.0, 35.0, 25.0, 35.0, 20.0, 45.0, 10.0],
-#             "term_is_pre_cohort": [
-#                 True,
-#                 False,
-#                 False,
-#                 False,
-#                 False,
-#                 False,
-#                 False,
-#                 False,
-#             ],
-#         },
-#     ).astype(
-#         {
-#             "student_id": "string",
-#             "cohort_id": "string",
-#             "term_id": "string",
-#             "enrollment_intensity": "string",
-#         }
-#     )
+@pytest.fixture(scope="module")
+def df_test():
+    return pd.DataFrame(
+        {
+            "student_id": ["01", "01", "01", "02", "02", "03", "04", "05"],
+            "term_rank": [3, 4, 5, 5, 6, 2, 4, 8],
+            "cohort_id": [
+                "2020-21 SPRING",
+                "2020-21 SPRING",
+                "2020-21 SPRING",
+                "2021-22 FALL",
+                "2021-22 FALL",
+                "2019-20 FALL",
+                "2020-21 SPRING",
+                "2022-23 FALL",
+            ],
+            "term_id": [
+                "2020-21 FALL",
+                "2020-21 SPRING",
+                "2021-22 FALL",
+                "2021-22 FALL",
+                "2023-24 FALL",
+                "2019-20 SPRING",
+                "2020-21 SPRING",
+                "2022-23 FALL",
+            ],
+            "enrollment_year": [1, 1, 2, 1, 3, 1, 1, 1],
+            "enrollment_intensity": [
+                "FULL-TIME",
+                "FULL-TIME",
+                "FULL-TIME",
+                "PART-TIME",
+                "PART-TIME",
+                "PART-TIME",
+                "FULL-TIME",
+                pd.NA,
+            ],
+            "num_credits_earned": [25.0, 30.0, 35.0, 25.0, 35.0, 20.0, 45.0, 10.0],
+            "term_is_pre_cohort": [
+                True,
+                False,
+                False,
+                False,
+                False,
+                False,
+                False,
+                False,
+            ],
+        },
+    ).astype(
+        {
+            "student_id": "string",
+            "cohort_id": "string",
+            "term_id": "string",
+            "enrollment_intensity": "string",
+        }
+    )
 
 
 # @pytest.mark.parametrize(
