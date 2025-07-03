48d46f5ecee7ca8d314880f004d0ca1d65daecb3
nm3224 <nm3224>
2025-07-03 14:15:02 +0000

    linter passed

--- pipelines/pdp/institution_id/00-data-assessment-TEMPLATE.py
+++ pipelines/pdp/institution_id/00-data-assessment-TEMPLATE.py
@@ -650,11 +650,11 @@ df_pct_creds_by_yr.groupby("year_of_enrollment")[
 for bar in ax.patches:
     height = bar.get_height()
     ax.annotate(
-        f"{height:.1f}%",  
+        f"{height:.1f}%",
         (bar.get_x() + bar.get_width() / 2, height),
-        ha='center',
-        va='bottom',
-        fontsize=10
+        ha="center",
+        va="bottom",
+        fontsize=10,
     )
 
 plt.tight_layout()
