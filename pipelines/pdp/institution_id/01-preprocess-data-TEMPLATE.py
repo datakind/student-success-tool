c63fb3b24fcb27cf907a9cdfda5fa86e2536827b
nm3224 <nm3224>
2025-07-15 18:57:11 +0000

    fixing linter

--- pipelines/pdp/institution_id/01-preprocess-data-TEMPLATE.py
+++ pipelines/pdp/institution_id/01-preprocess-data-TEMPLATE.py
@@ -143,11 +143,9 @@ df_student_terms.columns.tolist()
 # We usually drop pre-cohort course records; If school requests otherwise, please set include_pre_cohort_courses in your config to TRUE, re-load the config, THEN run this cell!
 
 if cfg.preprocessing.include_pre_cohort_courses == False:
-    df_student_terms = df_student_terms[
-        df_student_terms["term_is_pre_cohort"] == False
-    ]
+    df_student_terms = df_student_terms[df_student_terms["term_is_pre_cohort"] == False]
 
-# sanity check; should be 0 True 
+# sanity check; should be 0 True
 df_student_terms["term_is_pre_cohort"].value_counts(dropna=False)
 
 # COMMAND ----------
