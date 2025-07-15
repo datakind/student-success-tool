dea2c21842f5226cdea8d42eac1dba8a93e7d4cd
vishpillai123 <vishpillai123>
2025-07-15 16:25:20 +0000

    ok fixing package path

--- tests/reporting/template/test_custom_template_context.py
+++ tests/reporting/template/test_custom_template_context.py
@@ -169,10 +169,10 @@ def dummy_custom_config():
 
 
 @patch("student_success_tool.reporting.sections.registry.SectionRegistry.render_all")
-@patch("student_success_tool.reporting.model_card.base.CustomModelCard.collect_metadata")
-@patch("student_success_tool.reporting.model_card.base.CustomModelCard.load_model")
-@patch("student_success_tool.reporting.model_card.base.CustomModelCard.extract_training_data")
-@patch("student_success_tool.reporting.model_card.base.CustomModelCard.find_model_version")
+@patch("student_success_tool.reporting.model_card.custom.CustomModelCard.collect_metadata")
+@patch("student_success_tool.reporting.model_card.custom.CustomModelCard.load_model")
+@patch("student_success_tool.reporting.model_card.custom.CustomModelCard.extract_training_data")
+@patch("student_success_tool.reporting.model_card.custom.CustomModelCard.find_model_version")
 def test_custom_school_model_card_template_placeholders_filled(
     mock_find_version,
     mock_extract_data,
