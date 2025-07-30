584d5f30c9752599e0f2c7afa938950885cb62aa
nm3224 <nm3224>
2025-07-30 17:26:47 +0000

    adding course grades by delivery method

--- pipelines/pdp/institution_id/00-data-assessment-TEMPLATE.py
+++ pipelines/pdp/institution_id/00-data-assessment-TEMPLATE.py
@@ -588,6 +588,11 @@ print(df_cohort_filtered["enrollment_type"].value_counts(dropna=False))
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Cohort File
+
+# COMMAND ----------
+
 ax = sb.histplot(
     df_cohort_filtered.sort_values("cohort"),
     y="cohort",
@@ -761,6 +766,11 @@ plt.show()
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Course File
+
+# COMMAND ----------
+
 ax = sb.histplot(
     df_course_filtered.sort_values(by="academic_year"),
     y="academic_year",
@@ -770,6 +780,66 @@ ax = sb.histplot(
     edgecolor="white",
 )
 _ = ax.set(xlabel="Number of Course Enrollments")
+
+# COMMAND ----------
+
+# course enrollments by enrollment intensity
+
+ax = sb.histplot(
+    pd.merge(
+        df_course_filtered.groupby(cfg.student_id_col)
+        .size()
+        .rename("num_courses_enrolled")
+        .reset_index(drop=False),
+        df_cohort_filtered[[cfg.student_id_col, "enrollment_intensity_first_term"]],
+        on=cfg.student_id_col,
+        how="inner",
+    ),
+    x="num_courses_enrolled",
+    hue="enrollment_intensity_first_term",
+    multiple="stack",
+    binwidth=5,
+    edgecolor="white",
+)
+ax.set(xlabel="Number of courses enrolled (total)", ylabel="Number of students")
+sb.move_legend(ax, loc="upper right", bbox_to_anchor=(1, 1))
+
+# COMMAND ----------
+
+# grades by delivery method 
+print(df_course_filtered["grade"].value_counts(dropna=False).sort_index())
+
+# Map letter grades A and F to numeric; ignores P, I, W
+# confirm this with school! 
+df_numeric_grades = df_course_filtered.copy()
+letter_to_num = {"A": 4.0, "F": 0.00}
+df_numeric_grades["grade_converted"] = pd.to_numeric(
+    df_numeric_grades["grade"], errors="coerce"
+).fillna(df_numeric_grades["grade"].map(letter_to_num))
+
+# Keep only rows where 'grade' is numeric
+df_numeric_grades = df_numeric_grades [
+    pd.to_numeric(df_numeric_grades["grade_converted"], errors="coerce").notna()
+]
+
+# Ensure float dtype for plotting
+df_numeric_grades["grade_converted"] = df_numeric_grades["grade_converted"].astype(float)
+
+print(df_numeric_grades["grade_converted"].value_counts(dropna=False).sort_index())
+
+ax = sb.histplot(
+    df_numeric_grades,
+    x="grade_converted",
+    hue="delivery_method",
+    multiple="stack",
+    binwidth=1,
+    binrange=(0, 5),            
+    edgecolor="white"
+)
+
+ax.set(xlabel="Course grade", ylabel="Number of course enrollments")
+plt.title("Course Grades by Delivery Method")
+plt.show()
 
 # COMMAND ----------
 
@@ -792,6 +862,22 @@ num_courses = (
 )
 num_subjects = df_course_filtered["course_cip"].nunique()
 print(f"{num_courses} distinct courses, {num_subjects} distinct subjects")
+
+# COMMAND ----------
+
+jg = sb.jointplot(
+    df_course_filtered.groupby(cfg.student_id_col).agg(
+        {"number_of_credits_attempted": "sum", "number_of_credits_earned": "sum"}
+    ),
+    x="number_of_credits_attempted",
+    y="number_of_credits_earned",
+    kind="hex",
+    joint_kws={"bins": "log"},
+    marginal_kws={"edgecolor": "white"},
+    ratio=4,
+)
+jg.refline(y=120.0)  # or whichever num credits earned is a relavent benchmark
+jg.set_axis_labels("Number of Credits Attempted", "Number of Credits Earned")
 
 # COMMAND ----------
 
@@ -940,43 +1026,6 @@ df_cohort_filtered[["race", "pell_status_first_year"]].groupby("race").value_cou
 
 # MAGIC %md
 # MAGIC And so on, and so forth.
-
-# COMMAND ----------
-
-ax = sb.histplot(
-    pd.merge(
-        df_course_filtered.groupby(cfg.student_id_col)
-        .size()
-        .rename("num_courses_enrolled")
-        .reset_index(drop=False),
-        df_cohort[[cfg.student_id_col, "enrollment_intensity_first_term"]],
-        on=cfg.student_id_col,
-        how="inner",
-    ),
-    x="num_courses_enrolled",
-    hue="enrollment_intensity_first_term",
-    multiple="stack",
-    binwidth=5,
-    edgecolor="white",
-)
-ax.set(xlabel="Number of courses enrolled (total)", ylabel="Number of students")
-sb.move_legend(ax, loc="upper right", bbox_to_anchor=(1, 1))
-
-# COMMAND ----------
-
-jg = sb.jointplot(
-    df_course_filtered.groupby(cfg.student_id_col).agg(
-        {"number_of_credits_attempted": "sum", "number_of_credits_earned": "sum"}
-    ),
-    x="number_of_credits_attempted",
-    y="number_of_credits_earned",
-    kind="hex",
-    joint_kws={"bins": "log"},
-    marginal_kws={"edgecolor": "white"},
-    ratio=4,
-)
-jg.refline(y=120.0)  # or whichever num credits earned is a relavent benchmark
-jg.set_axis_labels("Number of Credits Attempted", "Number of Credits Earned")
 
 # COMMAND ----------
 
