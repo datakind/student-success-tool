a933038e6d94384c970e2438d02c61d58301b833
Vishakh Pillai <vishpillai97@gmail.com>
2025-07-31 17:59:13 -0400

    removing init import

--- src/student_success_tool/dataio/__init__.py
+++ src/student_success_tool/dataio/__init__.py
@@ -1,5 +1,5 @@
 from . import converters, pdp, schemas
-from .assets import read_config, read_features_table, write_config
+from .assets import read_config, read_features_table
 from .models import load_mlflow_model, patch_mlflow_model_env_files
 from .read import from_csv_file, from_delta_table
 from .write import to_delta_table
