c74687362c867590e94015466fc91488f050b82c
vishpillai123 <vishpillai123>
2025-08-26 20:06:13 +0000

    adding safe init into templates

--- pipelines/pdp/institution_id/h2o/02-train-h2o-model-TEMPLATE.py
+++ pipelines/pdp/institution_id/h2o/02-train-h2o-model-TEMPLATE.py
@@ -49,12 +49,10 @@ from student_success_tool.modeling import h2o_modeling
 from student_success_tool.configs import h2o_configs
 
 import h2o
+h2o_modeling.utils.safe_h2o_init()
 
 # HACK: Disable the mlflow widget template otherwise it freaks out
 os.environ["MLFLOW_ENABLE_ARTIFACTS_PROGRESS_BAR"] = "false"
-
-logging.info("Starting H2O cluster...")
-h2o.init()
 
 # COMMAND ----------
 
