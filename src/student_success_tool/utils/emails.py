6e73c755ce6a405e4a2964bcefce22cfb93c2a79
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-09-01 14:47:11 -0700

    Add hello

--- src/student_success_tool/utils/emails.py
+++ src/student_success_tool/utils/emails.py
@@ -9,6 +9,8 @@ SMTP_SERVER = "smtp.mandrillapp.com"
 SMTP_PORT = 587  # or 465 for SSL
 COMPLETION_SUCCESS_SUBJECT = "Edvise: Inference Results Available"
 COMPLETION_SUCCESS_MESSAGE = """\
+    Hello!
+    
     Your most recent inference results are now available in Edvise. Please log in and download them at your earliest convenience. As a reminder, you should have the StudyID appended file from the NSC SFTP to reconnect our inferences to your students and begin supporting those most in need of intervention.
 
     We are glad to help consult on anything you need clarification on - should new features have made themselves known or other questions arise, do not hesitate to reach out.
