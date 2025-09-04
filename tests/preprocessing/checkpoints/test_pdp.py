8ecd4dfdde18d2da8fd6b6fd91ac6677a9922739
nm3224 <nm3224>
2025-09-04 17:32:13 +0000

    fixed unit test

--- tests/preprocessing/checkpoints/test_pdp.py
+++ tests/preprocessing/checkpoints/test_pdp.py
@@ -445,7 +445,7 @@ def test_first_student_terms_at_num_credits_earned(
                     "term_is_pre_cohort": [False, False, False, False, False],
                     "term_is_core": [True, True, True, True, True],
                 },
-                index=pd.Index([1, 3, 5, 6, 7], dtype="int64"),
+                index=pd.Index([1, 4, 7, 9, 10], dtype="int64"),
             ).astype({"student_id": "string"}),
         ),
     ],
